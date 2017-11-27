7b51aeebc2fc999d935cfbcfabc9fe876815ec9e
bartoszc <bartosz.cichowicz@kpit.com>
2017-11-27 12:30:52 +0100

    Update reposotory according chapter 10

--- src/com/luv2code/springdemo/RESTFortuneService.java
+++ src/com/luv2code/springdemo/RESTFortuneService.java
@@ -3,11 +3,12 @@ package com.luv2code.springdemo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HappyFortuneService implements FortuneService {
+public class RESTFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -8,25 +11,41 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
 	@Autowired
-	@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
-	/*@Autowired //opcjonalny
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService=theFortuneService;
-	}*/
-	
-	// default constructor
+	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
 	
-	/*@Autowired
-	public void doSomeCrazyStuff(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
-		this.fortuneService = fortuneService;
-	}*/
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");		
+	}
+	
+	// define a setter method
+	/*
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		this.fortuneService = theFortuneService;
+	}
+	*/
+	
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
